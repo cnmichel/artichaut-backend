f0e636daa958b4cd92339473e5cef4fd77089170
walczak <adrien.walczak@le-campus-numerique.fr>
2023-02-02 09:00:24 +0100

    (task): Adding relationships for Sitemaps Promos and Socials

--- app/Models/Promo.php
+++ app/Models/Promo.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Promo extends Model
 {
     use HasFactory;
+
+    public function langs()
+    {
+        return $this->hasMany(langs::class);
+    }
 }
--- app/Models/Sitemap.php
+++ app/Models/Sitemap.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Sitemap extends Model
 {
     use HasFactory;
+
+    public function langs()
+    {
+        return $this->hasMany(langs::class);
+    }
 }
