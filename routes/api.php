7c904f2482cc519630b421f7da5403309dc2a7c5
cnmichel <michel.nguyen@le-campus-numerique.fr>
2023-02-02 15:45:42 +0100

    refactor: code tidying up

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,19 @@
 <?php
 
-use App\Http\Controllers\API\FeatureController;
-use App\Http\Controllers\API\LangController;
-use App\Http\Controllers\API\ProductController;
-use App\Http\Controllers\API\PromoController;
-use App\Http\Controllers\API\SitemapController;
-use App\Http\Controllers\API\SocialController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\LangController;
 use App\Http\Controllers\API\RoleController;
 use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\API\CustomerController;
 use App\Http\Controllers\API\ReviewController;
 use App\Http\Controllers\API\HeroController;
 use App\Http\Controllers\API\ArticleController;
+use App\Http\Controllers\API\FeatureController;
+use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\PromoController;
+use App\Http\Controllers\API\SitemapController;
+use App\Http\Controllers\API\SocialController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,16 +30,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::apiResource('langs', LangController::class);
 Route::apiResource('roles', RoleController::class);
 Route::apiResource('users', UserController::class);
 Route::apiResource('customers', CustomerController::class);
 Route::apiResource('reviews', ReviewController::class);
 Route::apiResource('heroes', HeroController::class);
 Route::apiResource('articles', ArticleController::class);
-
 Route::apiResource('features', FeatureController::class);
 Route::apiResource('products', ProductController::class);
-Route::apiResource('socials', SocialController::class);
-Route::apiResource('sitemaps', SitemapController::class);
 Route::apiResource('promos', PromoController::class);
-Route::apiResource('langs', LangController::class);
+Route::apiResource('sitemaps', SitemapController::class);
+Route::apiResource('socials', SocialController::class);
