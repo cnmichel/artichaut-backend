00b4b8d91e1d8b87c50ecd5561eafa34f1a47964
walczak <adrien.walczak@le-campus-numerique.fr>
2023-02-02 11:11:34 +0100

    (tack): Add databaseseeder update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,9 @@ class DatabaseSeeder extends Seeder
             LangSeeder::class,
             FeatureSeeder::class,
             ProductSeeder::class,
+            SitemapSeeder::class,
+            Social::class,
+            Promo::class,
         ]);
     }
 }
