9480a50671e1b795f5c40cba18d70533117d50f9
cnmichel <michel.nguyen@le-campus-numerique.fr>
2023-02-02 09:44:11 +0100

    chore: replaced news table with articles table

--- database/migrations/2023_02_02_083717_create_articles_table.php
+++ database/migrations/2023_02_02_083717_create_articles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('news', function (Blueprint $table) {
+        Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('title', 255);
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('news');
+        Schema::dropIfExists('articles');
     }
 };
--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class NewsSeeder extends Seeder
+class ArticleSeeder extends Seeder
 {
     /**
      * Run the database seeds.
