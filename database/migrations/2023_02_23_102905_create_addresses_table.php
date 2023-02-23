f1db2fc49c9f122e131c5f4a4dcf83b1953db53e
Rodalljuhziz <adrien.walczak@le-campus-numerique.fr>
2023-02-23 11:47:34 +0100

    (task):Add new migrations sprint2

---
+++ database/migrations/2023_02_23_102905_create_addresses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name', 50);
+            $table->string('street', 255);
+            $table->string('city', 100);
+            $table->string('zip_code', 10);
+            $table->string('country', 100);
+            $table->foreignId('customer_id')->constrained('customers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
