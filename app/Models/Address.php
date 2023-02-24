c24ba83ce525d4884871e120662f674933c5412a
Rodalljuhziz <adrien.walczak@le-campus-numerique.fr>
2023-02-24 09:03:18 +0100

    (feature): Add factories, models on new tables

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Address extends Model
 {
     use HasFactory;
+    use HasFactory;
+
+    /**
+    * The attributes that are mass assignable.
+    *
+    * @var array<int, string>
+    */
+
+   protected $fillable = [
+       'name',
+       'street',
+       'city',
+       'zip_code',
+       'country',
+   ];
+
+   public function customer()
+   {       
+       return $this->belongsTo(Customer::class);
+
+   }
 }
